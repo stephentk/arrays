1f9332a4af0406a074107e353fea25cf18a265c2
stephentk <84783886+stephentk@users.noreply.github.com>
2021-11-17 00:46:29 -0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,38 @@
+
+/*
+* QUIZ REQUIREMENTS
+* - Your code should have a variable `softwareEngineeringStudents`
+* - The variable `softwareEngineeringStudents` should be an array containing the values of 10 students from your class
+* - Your code should print `softwareEngineeringStudents` to the console
+*
+*/
+var   softwareEngineeringStudents =  ["olu","pere","tobi","stephanie","stephen","tomiwa","toki","kelechi","stephanie","bloosom"]
+console.log( softwareEngineeringStudents)
+
+
+/*
+* QUIZ REQUIREMENTS
+* - Your code should have a variable `crew`
+* - The variable `crew` should be an array containing the Serenity\'s crew
+* - Your code should print `crew` to the console as an array. Do not iterate over the elements.
+*
+*/
+let captain = "Mal";
+let second = "Zoe";
+let pilot = "Wash";
+let companion = "Inara";
+let mercenary = "Jayne";
+let mechanic = "Kaylee";
+  var crew = ["captain","second","pilot","companion","mercrnary","mechanic"];
+  console.log(crew);
+  
+  /* QUIZ REQUIREMENTS
+* - Your code should have a variable `prices`
+* - The variable `prices` should be an array having different values for the 1st, 3rd, and 7th elements
+* - Your code should print `prices` to the console as an array. Do not iterate over the elements.
+*/
+const prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+prices[0] = 1.4;
+prices[3] = 2.5;
+prices[7] = 3.5;
+console.log(prices);
